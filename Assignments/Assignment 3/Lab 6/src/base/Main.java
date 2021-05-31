c87f24efd91b2e5a92ef6ddf03acdf5ee57ae3cd
Julia-Mangan <79725171+Julia-Mangan@users.noreply.github.com>
2021-05-31 00:05:05 -0400

    Lab 6

--- Assignments/Assignment 3/Lab 6/src/base/Main.java
+++ Assignments/Assignment 3/Lab 6/src/base/Main.java
@@ -3,13 +3,14 @@ package base;
 public class Main {
 
     public static void main(String[] args) {
-        Node first = new Node( value: 1);
-        Node second = new Node( value: 2);
-        Node third = new Node( value: 3);
-        Node fourth = new Node( value: 4);
+        Node first = new Node( 1);
+        Node second = new Node(  2);
+        Node third = new Node(  3);
+        Node fourth = new Node(  4);
 
         first.nextNode = second;
         second.nextNode = third;
         third.nextNode = fourth;
     }
 }
+// Main class
